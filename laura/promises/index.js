6c9ef3bdf5d2115bce686ad6af082047e51e3432
laura <lauraszm@gmail.com>
2020-12-16 22:54:49 +0000

    promise all - challenge

--- laura/promises/index.js
+++ laura/promises/index.js
@@ -25,27 +25,36 @@
 //     }
 // }
 
-let esPar = new Promise((resolve, reject) => {
-    console.log("procesando....")
-    setTimeout(() => {
-        let number = Math.floor(Math.random() * 10);
-            if(number % 2 == 0) {
-               resolve (number)
-            } else {
-                reject (number)
-            }
-    }, 4000);
+// let esPar = new Promise((resolve, reject) => {
+//     console.log("procesando....")
+//     setTimeout(() => {
+//         let number = Math.floor(Math.random() * 10);
+//             if(number % 2 == 0) {
+//                resolve (number)
+//             } else {
+//                 reject (number)
+//             }
+//     }, 4000);
        
-});
+// });
 
-esPar.then((result) => {
-    console.log("es par", result);
-})
-.catch(error => {
-    console.log("es impar", error)
-})
+// esPar.then((result) => {
+//     console.log("es par", result);
+// })
+// .catch(error => {
+//     console.log("es impar", error)
+// })
 
+const apiKey = "j4As5HO2OpUG2w2gTuuqQnIGuwOu2nnJ"
+function getApi(url){
+    return fetch(url)
+    .then((response) => response.json())
+    .then((res) => {
+        console.log(res);
+        // getFollowers(res);
+    }).catch(err =>{
+        console.log(err)
+    })
+}
 
-
-
-
+let resultado = getApi('https://api.giphy.com/v1/gifs/search' + '?api_key=' + apiKey)
